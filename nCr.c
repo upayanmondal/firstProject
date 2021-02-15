08562d059661bdd0e16603d7a36a8c23c5433d5e
upayanmondal <upamamondal@gmail.com>
2021-02-15 09:25:59 +0530

    combination

---
+++ nCr.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int factorial(int num)
+{
+	
+	int i,f=1;
+	
+	for(i=1; i<=num; i++)
+	{
+		
+		f=f*i;
+		
+		
+	}
+
+	
+	return (f);
+}
+
+int main()
+{
+	int num,r,nPr;
+	
+	printf("enter the total number and the value of r: \n");
+	scanf("%d%d",&num,&r);
+
+	
+	
+	
+	
+	
+	nPr = factorial(num)/(factorial(r)*factorial(num-r));
+	
+	printf("the value of nPr is: %d",nPr); 
+	
+	return 0;
+}
+
